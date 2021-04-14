7efc055ddd934dfb1d7cf428754fa7a3d02f2c41
bsmith175 <benlismith@gmail.com>
2021-04-14 01:20:18 -0400

    make_trainfile scrip

---
+++ make_trainfile.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+
+num_classes = 52
+suits = ["d", "h", "c", "s"]
+cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+def create(path="data/obj"):
+    lines = []
+    if (path == None):
+        path = "data/obj"
+
+    for filename in os.listdir(path):
+        if not filename.endswith('.jpg'): continue
+        fullname = os.path.join(path, filename)
+        lines.append(fullname)
+
+
+    print(f"Found {len(lines)} images")
+    f = open("./train.txt", "w")
+    print(f"Writing to file {f.name}")
+    f.writelines(lines)
+    f.close()
+
+# Create train.txt file 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", help="Path to the image directory, relative to darknet executable (default ./data/obj)")
+    args = parser.parse_args()
+    create(args.dir)
