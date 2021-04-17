a89edaa90a86b9efd823479c372da72dcb4124fc
bsmith175 <benlismith@gmail.com>
2021-04-17 16:12:40 -0400

    asdf

--- detect.py
+++ detect.py
@@ -184,7 +184,9 @@ draw = time.time()
 
 
 def write(x, results):
+    # top right corner
     c1 = tuple(x[1:3].int())
+    # bottom left corner
     c2 = tuple(x[3:5].int())
     img = results[int(x[0])]
     cls = int(x[-1])
@@ -194,7 +196,7 @@ def write(x, results):
     t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
     c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
     cv2.rectangle(img, c1, c2,color, -1)
-    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
+    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1)
     return img
 
 
