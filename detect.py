a1dc338f0e09012e49f0d33b737b2f7da506d624
bsmith175 <benlismith@gmail.com>
2021-04-18 20:36:22 -0400

    tweak

--- detect.py
+++ detect.py
@@ -14,33 +14,6 @@ import pickle as pkl
 import pandas as pd
 import random
 
-def arg_parse():
-    """
-    Parse arguements to the detect module
-    
-    """
-    
-    parser = argparse.ArgumentParser(description='YOLO v3 Detection Module')
-   
-    parser.add_argument("--images", dest = 'images', help = 
-                        "Image / Directory containing images to perform detection upon",
-                        default = "imgs", type = str)
-    parser.add_argument("--det", dest = 'det', help = 
-                        "Image / Directory to store detections to",
-                        default = "res", type = str)
-    parser.add_argument("--confidence", dest = "confidence", help = "Object Confidence to filter predictions", default = 0.5)
-    parser.add_argument("--nms_thresh", dest = "nms_thresh", help = "NMS Threshhold", default = 0.4)
-    parser.add_argument("--cfg", dest = 'cfgfile', help = 
-                        "Config file",
-                        default = "cfg/obj-yolov3.cfg", type = str)
-    parser.add_argument("--weights", dest = 'weightsfile', help = 
-                        "weightsfile",
-                        default = "weights/obj-yolov3_last.weights", type = str)
-    parser.add_argument("--reso", dest = 'reso', help = 
-                        "Input resolution of the network. Increase to increase accuracy. Decrease to increase speed",
-                        default = "608", type = str)
-    
-    return parser.parse_args()
 
 # returns imlist given relative path of image directory
 def get_imlist(images):
@@ -198,6 +171,34 @@ def draw_res(output, loaded_ims, classes, det_dir, imlist):
     map(cv2.imwrite, det_names, loaded_ims)
 
     torch.cuda.empty_cache()
+
+def arg_parse():
+    """
+    Parse arguements to the detect module
+    
+    """
+    
+    parser = argparse.ArgumentParser(description='YOLO v3 Detection Module')
+   
+    parser.add_argument("--images", dest = 'images', help = 
+                        "Image / Directory containing images to perform detection upon",
+                        default = "imgs", type = str)
+    parser.add_argument("--det", dest = 'det', help = 
+                        "Image / Directory to store detections to",
+                        default = "res", type = str)
+    parser.add_argument("--confidence", dest = "confidence", help = "Object Confidence to filter predictions", default = 0.5)
+    parser.add_argument("--nms_thresh", dest = "nms_thresh", help = "NMS Threshhold", default = 0.4)
+    parser.add_argument("--cfg", dest = 'cfgfile', help = 
+                        "Config file",
+                        default = "cfg/obj-yolov3.cfg", type = str)
+    parser.add_argument("--weights", dest = 'weightsfile', help = 
+                        "weightsfile",
+                        default = "weights/obj-yolov3_last.weights", type = str)
+    parser.add_argument("--reso", dest = 'reso', help = 
+                        "Input resolution of the network. Increase to increase accuracy. Decrease to increase speed",
+                        default = "608", type = str)
+    
+    return parser.parse_args()
 
 def main():
     args = arg_parse()
